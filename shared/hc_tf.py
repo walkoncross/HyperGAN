949097cafe8aa71888daa304bf2dc7916045cc1e
Martyn Garcia <martyn@255bits.com>
2016-09-05 16:18:26 -0600

    [feature] 256x160 checkpoint for https://hyperchamber.255bits.com/ba3051e944601e2d98b601f3347df0b1/40k_overfit_3:1.2/samples/42bdcbb55a9ecd043ffc3f2fc87f60bf

--- shared/hc_tf.py
+++ shared/hc_tf.py
@@ -265,7 +265,7 @@ def residual_block_deconv(result, activation, batch_size,id,name, output_channel
         left = deconv2d(result, output_shape, name=name+'l', k_w=3, k_h=3, d_h=1, d_w=1)
         left = batch_norm(batch_size, name=name+'bn')(left)
         left = activation(left)
-        left = deconv2d(left, output_shape, name=name+'l2', k_w=3, k_h=3, d_h=1, d_w=1)
+        left = deconv2d(left, output_shape, name=name+'l2', k_w=1, k_h=1, d_h=1, d_w=1)
         right = deconv2d(result, output_shape, name=name+'r', k_w=3, k_h=3, d_h=1, d_w=1)
     elif(id=='identity'):
         output_shape = s
@@ -276,7 +276,7 @@ def residual_block_deconv(result, activation, batch_size,id,name, output_channel
         left = deconv2d(left, output_shape, name=name+'l', k_w=3, k_h=3, d_h=1, d_w=1)
         left = batch_norm(batch_size, name=name+'bn2')(left)
         left = activation(left)
-        left = deconv2d(left, output_shape, name=name+'l2', k_w=3, k_h=3, d_h=1, d_w=1)
+        left = deconv2d(left, output_shape, name=name+'l2', k_w=1, k_h=1, d_h=1, d_w=1)
         right = result
     elif(id=='deconv'):
         output_shape = [s[0], s[1]*stride, s[2]*stride,s[3]//stride]
@@ -290,6 +290,6 @@ def residual_block_deconv(result, activation, batch_size,id,name, output_channel
         left = deconv2d(left, output_shape, name=name+'l', k_w=stride+1, k_h=stride+1, d_h=stride, d_w=stride)
         left = batch_norm(batch_size, name=name+'lbn')(left)
         left = activation(left)
-        left = deconv2d(left, output_shape, name=name+'l2', k_w=3, k_h=3, d_h=1, d_w=1)
+        left = deconv2d(left, output_shape, name=name+'l2', k_w=1, k_h=1, d_h=1, d_w=1)
         right = deconv2d(right, output_shape, name=name+'r', k_w=stride+1, k_h=stride+1, d_h=stride, d_w=stride)
     return left+right
